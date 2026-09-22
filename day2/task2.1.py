d35e3aa272a5c2c0f02117b5621abb13329e0748
Stephan Kenmegne Kamdem <stephan.kenmegne-kamdem@epitech.eu>
2026-09-22 16:41:29 +0200

    final version day 2

--- day2/task2.1.py
+++ day2/task2.1.py
@@ -1,29 +1,23 @@
-ans1=1+11+111+1111+11111+111111+1111111+11111111+111111111
-print(ans1)
-for k in range(6):
-    if k == 0:
-       print()
-    else: 
-       print(pow(ans1,k))
-ans2=1+11+111+1111+11111+111111+1111111+11111111+111111111+1111111111 
-for l in range(6):
-    if l == 0:
-       print()
-    else:
-       print(pow(ans2,l))
-ans3=1+11+111+1111+11111+111111+1111111+11111111+111111111+1111111111+11111111111
-for m in range(6):
-    if m == 0:
-       print()
-    else:
-       print(pow(ans3,m))
-
-num1 = 0
-for i in range(10):
+def sum(n):
+ num1 = 0
+ for i in range(n):
      for j in range(i):
-        print (num1)
         num1=num1+pow(10,j)
-        print (num1)
-    
-   
-print (num1)
+ return num1
+def power(n, i):
+    for j in range(1, i):
+      print(pow(n,j+1))
+ans1=sum(10)
+print(ans1)
+power(ans1, 5)
+ans2=sum(11)
+print(ans2)
+power(ans2, 5)
+ans3=sum(12)
+print(ans3)
+power(ans3, 5)
+
+
+
+
+
